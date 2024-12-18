679e1e65af8c3511129b919cf93d5494f3b70f17
nandoide <nangdoide@gmail.com>
2024-12-18 18:35:07 +0100

    Refactor compile script for improved macOS compatibility; add functions for GMP and MPFR compilation and checks

--- compile.sh
+++ compile.sh
@@ -1,49 +1,55 @@
 #!/bin/bash
-#Ubuntu compilation
-# Bind the destination to a shell variable
-if command -v nproc &> /dev/null
-then
-    NPROC=$(nproc)
-else
-    NPROC=$(sysctl -n hw.ncpu)
-fi
 
-# rm -rf ~/CODE/out
-mkdir ~/CODE/out
-# Navigate to working folder
-cd ~/CODE
-# rm -rf gmp-6.3.0
-# rm -rf mpfr-4.2.1
-# rm gmp-6.3.0.tar.xz
-# rm mpfr-4.2.1.tar.bz2
-
-export MPDIR=~/CODE/out
-# Download and unpack gmp
-# curl -O -k https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz
-# tar Jxf gmp-6.3.0.tar.xz
-# Download and unpack mpfr
-#curl -O https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.bz2
-# cp ~/CODE/mpfr-4.2.1.tar.bz2 ./
-# tar jxf mpfr-4.2.1.tar.bz2
-
-# Obtener la ruta de instalación de libomp
 LIBOMP_PREFIX=$(brew --prefix libomp)
 
-export CFLAGS="-O2 -pedantic -fomit-frame-pointer -m64 -fPIC -fopenmp -fthread-jumps" 
-export PATH="${LIBOMP_PREFIX}/bin:$PATH"
-export LDFLAGS="-L${LIBOMP_PREFIX}/lib"
-export CPPFLAGS="-I${LIBOMP_PREFIX}/include"
-export CC="gcc -std=gnu11"
+export CFLAGS="-O2 -pedantic -fomit-frame-pointer -m64 -fPIC -fopenmp"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export LIBRARY_PATH="/opt/homebrew/opt/llvm/lib:$LIBRARY_PATH"
+export CC="clang"
 
-cd ~/CODE/gmp-6.3.0
+NPROC=$(sysctl -n hw.ncpu)
 
-./configure --enable-static=yes --enable-shared=yes --with-pic=yes
-make clean
-make -j$NPROC
+compile_gmp() {
+    cd ~/CODE/gmp-6.3.0
+    ./configure --enable-static=yes --enable-shared=yes --with-pic=yes
+    make clean
+    make -j$NPROC
+}
 
+compile_mpfr() {
+    cd ~/CODE/mpfr-4.2.1
+    ./configure --enable-static=yes --enable-shared=yes --with-pic=yes --with-gmp=~/CODE/gmp-6.3.0
+    make clean
+    make -j$NPROC
+}
 
-# cd ~/CODE/mpfr-4.2.1
-# make clean
-# ./configure  --enable-static=yes --enable-shared=yes --with-pic=yes --with-gmp=~/CODE/gmp-6.3.0
-# make -j$NPROC
-# make check
+check_gmp() {
+    cd ~/CODE/gmp-6.3.0
+    make check
+}
+
+check_mpfr() {
+    cd ~/CODE/mpfr-4.2.1
+    make check
+}
+
+case "$1" in
+    gmp)
+        compile_gmp
+        ;;
+    mpfr)
+        compile_mpfr
+        ;;
+    gmp_check)
+        check_gmp
+        ;;
+    mpfr_check)
+        check_mpfr
+        ;;
+    *)
+        echo "Usage: $0 {gmp|mpfr|gmp_check|mpfr_check}"
+        exit 1
+        ;;
+esac
